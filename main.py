6609020405b90d12006d67728cee88899fb97b23
Rose Jen <rose@rosescrap.com>
2011-04-17 15:31:28 -0700

    basic html + css

--- main.py
+++ main.py
@@ -14,20 +14,60 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import os
+from google.appengine.ext.webapp import template
+
+import cgi
+
+from google.appengine.api import users
 from google.appengine.ext import webapp
-from google.appengine.ext.webapp import util
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext import db
 
+class Greeting(db.Model):
+    author = db.UserProperty()
+    content = db.StringProperty(multiline=True)
+    date = db.DateTimeProperty(auto_now_add=True)
 
-class MainHandler(webapp.RequestHandler):
+class MainPage(webapp.RequestHandler):
     def get(self):
-        self.response.out.write('Hello world!')
+        greetings_query = Greeting.all().order('-date')
+        greetings = greetings_query.fetch(10)
 
+        if users.get_current_user():
+            url = users.create_logout_url(self.request.uri)
+            url_linktext = 'LOGOUT'
+        else:
+            url = users.create_login_url(self.request.uri)
+            url_linktext = 'LOGIN'
+
+        template_values = {
+            'greetings': greetings,
+            'url': url,
+            'url_linktext': url_linktext,
+        }
+
+        path = os.path.join(os.path.dirname(__file__), 'index.html')
+        self.response.out.write(template.render(path, template_values))
+
+class Guestbook(webapp.RequestHandler):
+	 def post(self):
+	     greeting = Greeting()
+
+	     if users.get_current_user():
+	         greeting.author = users.get_current_user()
+
+	     greeting.content = self.request.get('content')
+	     greeting.put()
+	     self.redirect('/')
+
+application = webapp.WSGIApplication(
+	                                  [('/', MainPage),
+	                                   ('/sign', Guestbook)],
+	                                  debug=True)
 
 def main():
-    application = webapp.WSGIApplication([('/', MainHandler)],
-                                         debug=True)
-    util.run_wsgi_app(application)
+	 run_wsgi_app(application)
 
-
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+	 main()
